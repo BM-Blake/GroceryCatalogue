317cad738da62e7f90305d59bc8c11a50b603aa2
Blake <naywunnazaw0@gmail.com>
2025-04-25 20:49:27 +0700

    my_fork

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,13 +1,47 @@
 import React from 'react'
-import { Text,StyleSheet} from 'react-native';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import DairyScreen from '../screens/DairyScreen';
+import FruitsScreen from '../screens/FruitsScreen';
+import VegetablesScreen from '../screens/VegetablesScreen';
+
+const Tab = createBottomTabNavigator();
 
 function TabNavigator() {
   return (
     <SafeAreaProvider>
-      <Text>Tab Navigator</Text>
+      <NavigationContainer>
+        <SafeAreaView style={styles.safeArea}>
+          <Text style={styles.brandName}>FreshMart</Text>
+          <Tab.Navigator>
+            <Tab.Screen
+              name="Fruits"
+              component={FruitsScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Tab.Screen
+              name="Vegetables"
+              component={VegetablesScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Tab.Screen
+              name="Dairy"
+              component={DairyScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+          </Tab.Navigator>
+        </SafeAreaView>
+      </NavigationContainer>
     </SafeAreaProvider>
-    
+
   )
 }
 
@@ -15,12 +49,12 @@ export default TabNavigator
 
 const styles = StyleSheet.create({
   container: { flex: 1, padding: 10 },
-  title: { fontSize: 27, marginBottom: 10, textAlign:'center',color: 'black', fontStyle:'italic'},
+  title: { fontSize: 27, marginBottom: 10, textAlign: 'center', color: 'black', fontStyle: 'italic' },
   itemContainer: { flexDirection: 'row', marginBottom: 10, alignItems: 'center' },
   image: { width: 120, height: 120, marginRight: 10 },
-  details: { marginLeft:40,alignItems:'center',justifyContent:'center',width:200 },
+  details: { marginLeft: 40, alignItems: 'center', justifyContent: 'center', width: 200 },
   name: { fontSize: 19 },
-  price: { color: 'green', fontWeight: 'bold'},
+  price: { color: 'green', fontWeight: 'bold' },
   addToCart: { color: 'blue', fontWeight: 'bold' },
   button: {
     backgroundColor: '#F48021',
@@ -31,7 +65,7 @@ const styles = StyleSheet.create({
   buttonText: {
     color: '#fff',
     // fontWeight: 'bold',
-    fontSize:13
+    fontSize: 13
   },
   safeArea: {
     flex: 1,
@@ -46,4 +80,4 @@ const styles = StyleSheet.create({
   }
 });
 
-export {styles}
+export { styles }
